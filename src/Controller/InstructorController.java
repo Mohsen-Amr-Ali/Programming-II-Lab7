56294ef3b982a31e226a304784d1860d01005709
yasseroosy <ahmed.yasser0113@gmail.com>
2025-11-16 01:28:52 +0200

    InstructorController Skeleton Build

--- src/Controller/InstructorController.java
+++ src/Controller/InstructorController.java
@@ -1,1 +1,14 @@
 //YASSER'S TERRITORY
+package Controller;
+import Model.Instructor;
+import Model.JsonDatabaseManager;
+
+public class InstructorController {
+    // Controller logic for Instructor
+    private JsonDatabaseManager dbManager;
+    private Instructor currentInstructor;
+    public InstructorController(JsonDatabaseManager dbManager, Instructor currentInstructor) {
+        this.dbManager = dbManager;
+        this.currentInstructor = currentInstructor;
+    }
+}
